60a6ea2101934e5423d72959dac39527c60cf040
Charles Antoine Fonvielle <ca.fonvielle@gmail.com>
2016-09-05 14:08:21 +0200

    tests JUnit

---
+++ pizzeria-console-maven/pizzeria-model/src/test/java/fr/pizzeria/model/ClientTest.java
@@ -0,0 +1,48 @@
+package fr.pizzeria.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.pizzeria.exception.CreditException;
+import fr.pizzeria.exception.DebitException;
+
+public class ClientTest {
+
+	public Client client;
+	
+	@Before
+	public void setUp(){
+		client = new Client(1, "", "", 200);
+	}
+	
+	@Test
+	public void testCrediterCompte() throws CreditException{
+		double montant = 50;
+		double soldeOld = client.getSolde();
+		client.crediterCompte(montant);
+		assertEquals(soldeOld+montant, client.getSolde(), 0);
+	}
+
+	@Test(expected = CreditException.class)
+	public void testFailCrediterCompte() throws CreditException{
+		double montant = 5000;
+		client.crediterCompte(montant);
+	}
+	
+	@Test
+	public void testDebiterCompte() throws DebitException {
+		double montant = 50;
+		double soldeOld = client.getSolde();
+		client.debiterCompte(montant);
+		assertEquals(soldeOld-montant, client.getSolde(), 0);
+	}
+
+	@Test(expected = DebitException.class)
+	public void testFailDebiterCompte() throws DebitException{
+		double montant = 500;
+		client.debiterCompte(montant);
+	}
+	
+}
